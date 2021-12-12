ef8808f614eed4635a7d336642692d0e1a74cc88
lvkunpeng@163.com <lvkunpeng@163.com>
2021-12-12 23:59:47 +0800

    'init'

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import store from "/@/store";
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(store)
+app.use(ElementPlus)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+function pathResolve(dir: string) {
+  return resolve(__dirname, ".", dir);
+}
+
+// export default defineConfig({
+  
+// })
+module.exports = {
+  plugins: [vue()],
+  alias: {
+    "/@/": pathResolve("src"),
+  },
+  optimizeDeps: {
+    include: ["@ant-design/icons-vue"],
+  },
+};
+
