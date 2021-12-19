cf440f59e7c1cb1f2c29cff533ce04579cd6ba51
lvkunpeng@163.com <lvkunpeng@163.com>
2021-12-19 21:50:46 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import store from "/@/store";
+// 这种引入利用别名的引入写法会引起ts报找不到模块的错误。这种需要在tsconfig文件中进行编译配置更改，加入对应的compilerOptions
+import store from "@/store";
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 const app = createApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite'
-import { resolve } from "path";
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+// 默认情况ts不认识一些模块包。但是也会去进行查找校验，此时需要手动引入改包或者模块的 @types声明包  eg: npm install @types/node --save-dev  从而识别此处的node模块
+import { resolve} from "path";
+import vue from "@vitejs/plugin-vue";
 
 function pathResolve(dir: string) {
   return resolve(__dirname, ".", dir);
 }
 
-// export default defineConfig({
-  
-// })
-module.exports = {
+export default defineConfig({
+  // 直接alias不被推荐了。需要放到resolve中
+  resolve: {
+    alias: {
+      "@": pathResolve("src"),
+    },
+  },
   plugins: [vue()],
-  alias: {
-    "/@/": pathResolve("src"),
-  },
-  optimizeDeps: {
-    include: ["@ant-design/icons-vue"],
-  },
-};
 
+});
